446244b131e4e4f41ad97cca86592ac4225999ca
ibalpalaloi <ibalpalaloi@gmail.com>
2022-01-29 19:54:34 +0800

    ubah password

--- app/Http/Controllers/UserBiodataController.php
+++ app/Http/Controllers/UserBiodataController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserBiodataController extends Controller
 {
     //
+    public function ubah_password(){
+        return view('user.ubah_password.ubah_password');
+    }
+
+    public function post_ubah_password(Request $request){
+        $user = User::where('id', Auth()->user()->id)->first();
+        if(!empty($user)){
+            $user->password = bcrypt($request->password);
+            $user->save();
+            return back()->with(['success', 'Password berhasil diubah']);
+        }
+        return back()->with(['error', 'User tidak di temukan']);
+    }
     
 }
