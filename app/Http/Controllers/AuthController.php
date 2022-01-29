452e32e60cef4c9a9ce5fd9d67d546a20cec8b39
ibalpalaloi <ibalpalaloi@gmail.com>
2022-01-29 15:11:23 +0800

    lupa password

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\SendPassword;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 use App\Models\Biodata;
 use App\Models\User_lupa_password;
 use Auth;
+use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
@@ -62,13 +64,42 @@ class AuthController extends Controller
         $password = $request->password;
 
         if(Auth::attempt(['no_telp' => $no_telp, 'password'=>$password])){
+            User_lupa_password::where('user_id', Auth()->user()->id)->delete();
             
             if(Auth()->user()->blokir == "true"){
                 return redirect('/user_login');
             }
             return redirect('/');
         }
+        $cek_lupa_password = $this->cek_lupa_password($no_telp, $password);
+        if($cek_lupa_password){
+            if(Auth::attempt(['no_telp' => $no_telp, 'password'=>$password])){
+                User_lupa_password::where('user_id', Auth()->user()->id)->delete();
+                
+                if(Auth()->user()->blokir == "true"){
+                    return redirect('/user_login');
+                }
+                return redirect('/');
+            }
+        }
         return back();
+    }
+
+    public function cek_lupa_password($no_telp, $password){
+        $user = User::where('no_telp', $no_telp)->first();
+        if(!empty($user)){
+            $cek_lupa_password = User_lupa_password::where([
+                ['user_id', $user->id],
+                ['password_baru', $password]
+            ])->first();
+            if(!empty($cek_lupa_password)){
+                $user->password = bcrypt($password);
+                $user->save();
+                return true;
+            }
+            return false;
+        }
+        return false;
     }
 
     public function admin_login(){
@@ -92,13 +123,40 @@ class AuthController extends Controller
     public function post_lupa_password(Request $request){
         $user = User::where('no_telp', $request->no_telp)->first();
         if(!empty($user)){
-            User_lupa_password::where('user_id', $user->id)->delete();
-            $lupa_password = new User_lupa_password;
-            $lupa_password->user_id = $user->id;
-            $lupa_password->save();
+            $cek_lupa_password = User_lupa_password::where('user_id', $user->id)->first();
+            if(empty($cek_lupa_password)){
+                $lupa_password = new User_lupa_password;
+                $lupa_password->user_id = $user->id;
+                $random_password = $this->random_password();
+                $lupa_password->password_baru = $random_password;
+                $lupa_password->save();
 
+                $this->kirim_email_password_baru($user->email, $random_password, $user->no_telp);
+                return redirect('/user_login')->with('success', 'Password Baru Akan DiKirimkan Ke Whatsapp Anda');
+            }
+            $this->kirim_email_password_baru($user->email, $cek_lupa_password->password_baru, $user->no_telp);
             return redirect('/user_login')->with('success', 'Password Baru Akan DiKirimkan Ke Whatsapp Anda');
         }
         return back()->with('error', 'Akun Tidak Tersedia');
+    }
+
+    public function random_password() {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+        $randomString = '';
+      
+        for ($i = 0; $i < 8; $i++) {
+            $index = rand(0, strlen($characters) - 1);
+            $randomString .= $characters[$index];
+        }
+      
+        return $randomString;
+    }
+
+    public function kirim_email_password_baru($email, $password, $no_telp){
+        $detail = [
+            'title'=>'nPassword Baru',
+            'body'=>'Password baru akun = '.$no_telp.', password baru = '.$password
+        ];
+        Mail::to($email)->send(new SendPassword($detail));
     }
 }
--- resources/views/admin/daftar_lupa_password.blade.php
+++ resources/views/admin/daftar_lupa_password.blade.php
@@ -28,7 +28,7 @@
                                   <td>{{$data->user->no_telp}}</td>
                                   <td>{{$data->created_at}}</td>
                                   <td>
-                                    <button type="button" onclick="" class="btn btn-primary">Kirim password</button>
+                                    <button type="button" onclick="kirim_pesan('{{$data->user->no_telp}}', '{{$data->password_baru}}')" class="btn btn-primary">Kirim password</button>
                                   </td>
                               </tr>
                           @endforeach
@@ -45,6 +45,11 @@
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script>
+    function kirim_pesan(no_telp, password){
+		var message = "Hallo Pelanggan dengan nomor = "+no_telp+" password baru anda = "+password;
 
+		var walink = 'https://wa.me/'+ "+62" + no_telp.substr(1, no_telp.length) +'?text=' + encodeURI(message);
+		window.open(walink);
+	}
 </script>
 @endsection
